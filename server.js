9cd3b9958874d9a4126b4d053d1e44a33b907a37
Julescesar-6 <votre-email@gmail.com>
2026-08-28 22:51:07 +0000

    Ajout Socket.IO tchat temps reel

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const path = require('path');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Serveur prêt sur le port ${PORT}`);
+// Gestion du temps réel
+io.on('connection', (socket) => {
+  socket.on('join', (username) => {
+    socket.username = username;
+    io.emit('system-message', `${username} a rejoint le chat`);
+  });
+
+  socket.on('chat-message', (data) => {
+    io.emit('chat-message', {
+      user: socket.username || 'Anonyme',
+      text: data
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      io.emit('system-message', `${socket.username} a quitté le chat`);
+    }
+  });
 });
+
+server.listen(PORT, () => console.log(`Serveur prêt sur le port ${PORT}`));
